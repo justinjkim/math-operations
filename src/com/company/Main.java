34cabc877dfa26f5140c7a087fe39b058f74bcd3
Justin Kim <jjkim928@gmail.com>
2017-09-11 14:47:48 -0400

    reformatted a little more nicely, and added division variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    Scanner input = new Scanner(System.in);
+        // write your code here
+        Scanner input = new Scanner(System.in);
 
-    System.out.println("Please enter a number: ");
-    String input1 = input.nextLine();
+        System.out.println("Please enter a number: ");
+        String input1 = input.nextLine();
 
-    System.out.printf("You have chosen %s. ", input1);
+        System.out.printf("You have chosen %s. ", input1);
 
-    System.out.println("Please enter a second number: ");
-    String input2 = input.nextLine();
-    System.out.printf("You have chosen %s.", input2);
+        System.out.println("Please enter a second number: ");
+        String input2 = input.nextLine();
+        System.out.printf("You have chosen %s.", input2);
 
-    Double operand1 = Double.parseDouble(input1);
-    Double operand2 = Double.parseDouble(input2);
+        Double operand1 = Double.parseDouble(input1);
+        Double operand2 = Double.parseDouble(input2);
 
-    double sum = operand1 + operand2;
-    System.out.println(sum);
+        double sum = operand1 + operand2;
+        System.out.println(sum);
 
+        double difference = operand1 - operand2;
+        System.out.println(difference);
+
+        double division = operand1 / operand2;
+        System.out.println(division);
 
     }
 }
